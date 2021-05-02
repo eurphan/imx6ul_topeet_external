943d498f386f3e9bb8df03942db5e35fca679d45
eurphan <eurphan@163.com>
2021-05-02 15:09:12 +0800

    [tslib]: 调整tslib安装目录

--- tslib-1.4/build.sh
+++ tslib-1.4/build.sh
@@ -8,11 +8,11 @@ if [ "$1" == "clean" ]; then
 	rm -rf out/
 else
 	./autogen.sh
-	./configure --host=arm-linux-gnueabihf ac_cv_func_malloc_0_nonnull=yes CC=arm-linux-gnueabihf-gcc CXX=arm-linux-gnueabihf-g++ --prefix=$(pwd)/../out/
+	./configure --host=arm-linux-gnueabihf ac_cv_func_malloc_0_nonnull=yes CC=arm-linux-gnueabihf-gcc CXX=arm-linux-gnueabihf-g++ --prefix=$(pwd)/../out/opt/tslib/
 	make -j4
 	make install
 	cd -
-	cp ts.conf out/etc/
+	cp ts.conf out/opt/tslib/etc/
 fi
 sync
 
